4eed68b613b43beb21f015850889004124132177
TyData29 <141240679+TyData29@users.noreply.github.com>
2025-08-11 14:39:14 +0200

    fix: correction de bugs lors de l'init dbt suite à test : à retester

--- install/dbt_setup.bat
+++ install/dbt_setup.bat
@@ -1,18 +1,18 @@
 @echo off
 echo Configuration et initialisation du projet DBT...
 
-REM Charger les variables d'environnement depuis set_env.ps1
-powershell -ExecutionPolicy Bypass -File set_env.ps1
+::REM Charger les variables d'environnement depuis set_env.ps1
+::powershell -ExecutionPolicy Bypass -File set_env.ps1
 
 
 REM Initialiser un nouveau projet DBT
 dbt init %DBT_PROJECT_NAME%
 
 REM Copier le fichier de configuration personnalisé
-copy "%CREDENTIALS_FILE_URL%" %PROJECT_NAME%\
+copy "%CREDENTIALS_FILE_URL%" %DBT_DIR%\
 
 REM Naviguer dans le répertoire du projet DBT
-cd %PROJECT_NAME%
+cd %DBT_DIR%
 
 REM Exécuter dbt debug pour vérifier la configuration
 dbt debug
